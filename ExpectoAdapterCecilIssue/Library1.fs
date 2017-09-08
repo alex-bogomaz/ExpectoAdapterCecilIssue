75350875f17f95e10b9a6f5beadec4702c152ecc
Oleksandr Bogomaz <alex_bogomaz@ukr.net>
2017-09-08 13:31:32 +0300

    added repro binaries

---
+++ ExpectoAdapterCecilIssue/Library1.fs
@@ -0,0 +1,20 @@
+﻿namespace ExpectoAdapterCecilIssue
+
+open Expecto
+
+module Tests =
+    (*[<Tests>]
+    let testSimpleTests =
+        testList "log record223" [
+            testCase "equality no optional write async" <| fun () -> ()
+            testCase "equality no optional write async2" <| fun () -> ()
+    ]*)
+
+    [<Tests>]
+    let equality =
+        testList "Aequality21"  [
+            testCase "equality 3" <| fun () ->
+                //Expect.isGreaterThan 3 3 ""
+                Expect.equal 42 42 ""
+                //Expect.isTrue false ""
+            ]
